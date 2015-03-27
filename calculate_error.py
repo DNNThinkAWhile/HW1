3869bbd35712ccd1ef5492274c1d7301d68bf3a7
weitang114 <weitang114@DarrendeMacBook-Pro.local>
2015-03-27 16:20:20 +0800

    FIX MAPPING
    map labels from train.lab to 48 indices

--- calculate_error.py
+++ calculate_error.py
@@ -18,31 +18,36 @@ def error_func_norm2(v1, v2):
 	df = np.array(d, dtype = 'f')
 	return norm2(df), norm2d(df)
 
-def read_label_map(file_48_39, file_state_48_39):
+def read_label_map(file_label, file_48_39):
 	d_1943_index = dict()
 	d_48_index = dict();
 	count = 0
+
+	# map phonemes(sil, aa,..) to their indices(int), 0:47
 	with open(file_48_39, 'r') as f:
 		for line in f:
-			count += 1
 			tokens = line.strip().split('\t')
 			d_48_index[tokens[0]] = count
+			count += 1
 
-	with open(file_state_48_39, 'r') as f:
+	# map labels(like "maeb0_si1411_1") to their phonemes' indices, 1943->48
+	with open(file_label, 'r') as f:
 		for line in f:
-			tokens = line.strip().split('\t')
-			d_1943_index[int(tokens[0])] = d_48_index[tokens[1]]
+			tokens = line.strip().split(',')
+			d_1943_index[tokens[0]] = d_48_index[tokens[1]]
+			
 	return d_1943_index
 
 def main():
 	print 'test calculate_error'
 
-	labelmap = read_label_map('48_39.map', 'state_48_39.map')
+	labelmap = read_label_map('train.lab', '48_39.map')
 	y_48 = np.random.random(48)
-	label = np.random.randomint(1943)
+	label = 'maeb0_si1411_3'
 
-	print 'y_48:' + str(y_48)
-	print 'label_1943:' + str(label)
+	# print 'y_48:' + str(y_48)
+	# print 'label_1943:' + str(label)
+	# print 'labelmap:' + str(labelmap)
 
 	print 'start'
 	err, errd = calculate_error(error_func_norm2, labelmap[label], y_48)
