005f16e862021a4111caf0723891ad9af1d26bdf
GBLin5566 <b00901056@ntu.edu.tw>
2015-03-30 16:04:00 +0800

    Update backpro.py
    remove the bugs and change the structure to fit the batch style

--- backpro.py
+++ backpro.py
@@ -1,3 +1,4 @@
+
 #from theano import * 
 import numpy as np
 #import theano.tensor as T
@@ -12,32 +13,30 @@ def backpropagate (gradC = np.zeros(1) , z  = [],  a = [] , layer = 0, theta = [
     WT = []
     W = theta[0]
     b = theta[1]
-    L = layer
-    if len(z) != len(W) or len(a) != len(b) or len(W) != L or len(b) != L :
-        print "Error List Size"
-        return
-
+    sizez = len(z)
     for Warray in W:
         WT.append(np.transpose(Warray))
 
-    for i in range(L - 1, -1, -1):
-        if i == L - 1 :
-            lum.append(sig(z[i])*gradC)
-            if i != 0 :
-                WC.append(a[i - 1]*lum[L - 1 - i])
-            else :
-                WC.append(data * lum[L - 1 - i])
-            C.append(Wans[i])
-            C.append(lum[i])
-        else:
-            lum.append(sig(z[i])*WT[i]*lum[L - i])
-            if i != 0 :
-                WC.append(a[i - 1]*lum[L - 1 - i])
-            else :
-                WC.append(data * lum[L - 1 - i])
-            C.append(Wans[i])
-            C.append(lum[i])
+	for j in sizez:
+		for i in range(layer - 1, -1, -1):
+			if i == layer - 1 :
+				lum.append(sig(z[j][i])*gradC)
+				if i != 0 :
+					WC.append(a[j][i - 1]*lum[layer - 1 - i])
+				else :
+					WC.append(featureset * lum[layer - 1 - i])
+				C.append(WC[i])
+				C.append(lum[i])
+			else:
+				lum.append(sig(z[j][i])*WT[i]*lum[layer - i])
+				if i != 0 :
+					WC.append(a[j][i - 1]*lum[layer - 1 - i])
+				else :
+					WC.append(featureset * lum[layer - 1 - i])
+				C.append(WC[i])
+				C.append(lum[i])
 
+				
     return C
        
 def main():
