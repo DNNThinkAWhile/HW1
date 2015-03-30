3a4a789bd5fa47fee85ef39da2007a0ea55b9c34
GBLin5566 <b00901056@ntu.edu.tw>
2015-03-31 00:03:18 +0800

    Update backpro.py
    fit average cal.

--- backpro.py
+++ backpro.py
@@ -6,18 +6,20 @@ import numpy as np
 def sig (x):
     return 1 / (1 + np.exp(-x))
 
-def backpropagate (gradC = np.zeros(1) , z  = [][],  a = [][] , layer = 0, theta = [], featureset = []):
+def backpropagate (gradC = np.zeros(1) , z  = [],  a = [] , layer = 0, theta = [], featureset = [], batchsize = 0):
     C = []
+    ans  = []
     WC = []
     lum = []
     WT = []
     W = theta[0]
     b = theta[1]
-    sizez = len(z)
     for Warray in W:
         WT.append(np.transpose(Warray))
 
-	for j in sizez:
+	for j in batchsize:
+		WC = []
+		lum = []
 		for i in range(layer - 1, -1, -1):
 			if i == layer - 1 :
 				lum.append(sig(z[j][i])*gradC)
@@ -25,18 +27,30 @@ def backpropagate (gradC = np.zeros(1) , z  = [][],  a = [][] , layer = 0, theta
 					WC.append(a[j][i - 1]*lum[layer - 1 - i])
 				else :
 					WC.append(featureset * lum[layer - 1 - i])
-				C.append(WC[i])
-				C.append(lum[i])
+				if j == 0:
+					C.append(WC[i])
+					C.append(lum[i])
+				else:
+					C[(layer - 1 - i) * 2] = C[(layer - 1 - i) * 2] + WC[i]
+					C[(layer - 1 - i) * 2 + 1] = C[(layer - 1 - i) * 2 + 1] + lum[i]
 			else:
 				lum.append(sig(z[j][i])*WT[i]*lum[layer - i])
 				if i != 0 :
 					WC.append(a[j][i - 1]*lum[layer - 1 - i])
 				else :
 					WC.append(featureset * lum[layer - 1 - i])
-				C.append(WC[i])
-				C.append(lum[i])
+				if j == 0 :
+					C.append(WC[i])
+					C.append(lum[i])
+				else:
+					C[(layer - 1 - i) * 2] = C[(layer - 1 - i) * 2] + WC[i]
+					C[(layer - 1 - i) * 2 + 1] = C[(layer - 1 - i) * 2 + 1] + lum[i]
 
-				
+
+	for i in layer - 1:
+		C[2*i] = C[2*i] / batchsize
+		C[2*i + 1] = C[2*i + 1] / batchsize
+
     return C
        
 def main():
