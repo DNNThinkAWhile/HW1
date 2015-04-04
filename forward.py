fd96171e46e2f399cee839c96a863d8a82f51cfd
weitang114.cml20 <weitang114@gmail.com>
2015-04-04 19:42:37 +0800

    fix same neuron number in layers crashing

--- forward.py
+++ forward.py
@@ -24,97 +24,99 @@ f_sigmoid = function([t],u)
 
 #return (W,B)
 def init(layer,neuron):
-   w_List = []
-   b_List = []
+   	w_List = []
+   	b_List = []
    
-   LAYER_NUM = layer;
-   MFCC_DIM = neuron[0]
-   NEURON_NUM = neuron[1:-1]
-   PHONE_NUM = neuron[-1]
+   	LAYER_NUM = layer;
+   	MFCC_DIM = neuron[0]
+   	NEURON_NUM = neuron[1:-1]
+   	PHONE_NUM = neuron[-1]
    # print 'layer:' + str(layer)
    # print 'neuron:' + str(neuron)
    # print 'MFCC_DIM:' + str(MFCC_DIM)
    # print 'NEURON_NUM:' + str(NEURON_NUM)
    # print 'PHONE_NUM:' + str(PHONE_NUM)
 
-   w1 = np.random.uniform(w_Min,w_Max,(NEURON_NUM[0],MFCC_DIM))
-   b1 = np.random.uniform(w_Min,w_Max,(NEURON_NUM[0]))
-   w_List.append(w1);
-   b_List.append(b1);
-   idx = 0
-   for iNeuron in NEURON_NUM:
-      if iNeuron == NEURON_NUM[-1]:
-         w_i = np.random.uniform(w_Min,w_Max,(PHONE_NUM,iNeuron))
-         b_i = np.random.uniform(w_Min,w_Max,(PHONE_NUM))
-      else:
-         w_i = np.random.uniform(w_Min,w_Max,(NEURON_NUM[idx+1],NEURON_NUM[idx]))
-         b_i = np.random.uniform(w_Min,w_Max,(NEURON_NUM[idx+1]))
-      w_List.append(w_i)
-      b_List.append(b_i)
-      idx += 1
+	w1 = np.random.uniform(w_Min,w_Max,(NEURON_NUM[0],MFCC_DIM))
+	b1 = np.random.uniform(w_Min,w_Max,(NEURON_NUM[0]))
+	w_List.append(w1);
+	b_List.append(b1);
+	idx = 0
+	for i in range(len(NEURON_NUM)):
+		iNeuron = NEURON_NUM[i]
+	  	if i == len(NEURON_NUM) - 1:
+			w_i = np.random.uniform(w_Min,w_Max,(PHONE_NUM,iNeuron))
+			b_i = np.random.uniform(w_Min,w_Max,(PHONE_NUM))
+		else:
+			w_i = np.random.uniform(w_Min,w_Max,(NEURON_NUM[idx+1],NEURON_NUM[idx]))
+	 		b_i = np.random.uniform(w_Min,w_Max,(NEURON_NUM[idx+1]))
+		w_List.append(w_i)
+		b_List.append(b_i)
+	 	idx += 1
    
-   rList = []
-   rList.append(w_List);
-   rList.append(b_List);
+	rList = []
+	rList.append(w_List);
+	rList.append(b_List);
    
-   return rList
+   	return rList
 
 
 def read_file(filePath):
-   List_speakID = []
-   List2D_MFCC_data = []
+   	List_speakID = []
+   	List2D_MFCC_data = []
    
-   with open(filePath, 'r') as f:
-       MFCC_trainFile = f.readlines()
-       num_lines = sum(1 for line in MFCC_trainFile)
-       for line in MFCC_trainFile:
-            partition = line.split()
-            List_speakID.append(partition[0])
-            List2D_MFCC_data.append(partition[1:])
+   	with open(filePath, 'r') as f:
+		MFCC_trainFile = f.readlines()
+		num_lines = sum(1 for line in MFCC_trainFile)
+		for line in MFCC_trainFile:
+			partition = line.split()
+			List_speakID.append(partition[0])
+			List2D_MFCC_data.append(partition[1:])
 
-   f.close()
+   	f.close()
    
-   return (List_speakID, List2D_MFCC_data)
+  	return (List_speakID, List2D_MFCC_data)
 
 
 ### get Count of "BATCH_SIZE" data to train for one time ### 
 
 def forward(List2D_MFCC_data, List_speakID, WandB, BATCH_SIZE, isTest):
-   w_List = WandB[0]
-   b_List = WandB[1]
-   a_List = []
-   z_List = []
-#   y_List = []
-   LAYER_NUM = len(w_List)
+	w_List = WandB[0]
+	b_List = WandB[1]
+	a_List = []
+	z_List = []
+	#   y_List = []
+	LAYER_NUM = len(w_List)
 
-   trainDataSetNum = len(List2D_MFCC_data) 
-   OneTime_train_set = []
-   OneTime_train_speechID = []
-   if isTest == 0:
-      for i in range(BATCH_SIZE):
-        idx = (int)(random.random()*trainDataSetNum)
-        arr = np.asfarray(List2D_MFCC_data[idx])
-        OneTime_train_set.append(arr)
-        OneTime_train_speechID.append(List_speakID[idx])
-   else:
-      for i in range(BATCH_SIZE):
-        arr = np.asfarray(List2D_MFCC_data[i])
-        OneTime_train_set.append(arr)
-        OneTime_train_speechID.append(List_speakID[i])
+	trainDataSetNum = len(List2D_MFCC_data) 
+	OneTime_train_set = []
+	OneTime_train_speechID = []
+	if isTest == 0:
+	  	for i in range(BATCH_SIZE):
+			idx = (int)(random.random()*trainDataSetNum)
+			arr = np.asfarray(List2D_MFCC_data[idx])
+			OneTime_train_set.append(arr)
+			OneTime_train_speechID.append(List_speakID[idx])
+   	else:
+	  	for i in range(BATCH_SIZE):
+			arr = np.asfarray(List2D_MFCC_data[i])
+			OneTime_train_set.append(arr)
+			OneTime_train_speechID.append(List_speakID[i])
 
-   for array in OneTime_train_set:
-      temp_a_List = []
-      temp_z_List = []
-      z_layer = array
-      for layer in range(0,LAYER_NUM,1):
-         z_layer = f_matrix_dot( w_List[layer] , z_layer , b_List[layer])
-         temp_z_List.append(z_layer)
-         z_layer = f_sigmoid(z_layer)
-         temp_a_List.append(z_layer)
-         
+   	for array in OneTime_train_set:
+		temp_a_List = []
+		temp_z_List = []
+		z_layer = array
+	  	for layer in range(0,LAYER_NUM,1):
+			#print str(w_List[layer].shape) + str(z_layer.shape) + str(b_List[layer].shape)
+			z_layer = f_matrix_dot( w_List[layer] , z_layer , b_List[layer])
+			temp_z_List.append(z_layer)
+			z_layer = f_sigmoid(z_layer)
+			temp_a_List.append(z_layer)
+		 
 #      y_List.append(z_layer)
-      a_List.append(temp_a_List)
-      z_List.append(temp_z_List)      
+		a_List.append(temp_a_List)
+		z_List.append(temp_z_List)      
    
 #   print(len(a_List))
 #   print(len(z_List))
@@ -130,7 +132,7 @@ def forward(List2D_MFCC_data, List_speakID, WandB, BATCH_SIZE, isTest):
 
    
 
-   return (OneTime_train_speechID, OneTime_train_set , a_List, z_List)
+	return (OneTime_train_speechID, OneTime_train_set , a_List, z_List)
 
 
 
