0277b0aa64a020ade7e2c48e94229139d30ba9c6
weitang114 <weitang114@DarrendeMacBook-Pro.local>
2015-04-10 12:21:35 +0800

    Predict: 48:39 map

--- predict.py
+++ predict.py
@@ -11,18 +11,18 @@ def predict_outfile(speech_id, Y):
    outfile_path = 'solution.csv'
    with open(outfile_path, 'w') as csvfile:
       writer = csv.writer(csvfile)
-      writer.writerow('Id,Prediction')
+      writer.writerow(['Id','Prediction'])
       for idx in range(len(Y)):
          sol = [str(speech_id[idx]), str(Y[idx])]
          writer.writerow(sol)
 
-def predict(test_file_path, theta, sol_map):
+def predict(test_file_path, theta, sol_map, sol_48_39_map):
 	speech_id, mfcc_data = read_file(test_file_path)
 	y_labs = []
 	selected_speech_id, feature_set, a_list, z_list = forward(mfcc_data, speech_id, theta, len(speech_id) ,len(speech_id), True)
         y_list = [a[-1] for a in a_list]
 	max_idx = [y.argmax(axis = 0) for y in y_list ]
-	y_labs = [ sol_map[idx] for idx in max_idx ]
+	y_labs = [ sol_48_39_map[sol_map[idx]] for idx in max_idx ]
 		# if speech_id != selected_speech_id:
 		#     print 'warning! debug!'
 	return speech_id, y_labs
@@ -38,6 +38,15 @@ def create_sol_map(map_48_39_file, labelnum):
 			count += 1
 	return sol_map
 
+def create_48_39_map(map_48_39_file):
+	d = dict()
+	with open(map_48_39_file, 'r') as f:
+		for line in f:
+			toks = line.strip().split('\t')
+			d[toks[0]] = toks[1]
+	print d
+	return d
+
 def main():
 	if ( len(sys.argv) != 2 ) :
 		print 'predict <model_id>'
@@ -46,11 +55,12 @@ def main():
 	label_num = 48
 	map_48_39_file = 'MLDS_HW1_RELEASE_v1/phones/48_39.map'
 	sol_map = create_sol_map(map_48_39_file, label_num)
+	sol_48_39_map = create_48_39_map(map_48_39_file)
 
-	load_model_path = 'model_' + str(sys.argv[1]) + '.npy'
+	load_model_path = str(sys.argv[1])
 	theta = load_model(load_model_path)
 	test_file_path = 'MLDS_HW1_RELEASE_v1/mfcc/test.ark'
-	speech_id, y_labs = predict(test_file_path, theta, sol_map)
+	speech_id, y_labs = predict(test_file_path, theta, sol_map, sol_48_39_map)
 	predict_outfile(speech_id, y_labs)
 
 if __name__ == '__main__':
